3ceb78e0e3fd1738fb7e1a79c756ea85af6636ca
malussuckow <marialuizassuckow@gmail.com>
2026-04-07 22:17:35 -0300

    protect SECRET_KEY with environment variable

--- core/settings.py
+++ core/settings.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 from pathlib import Path
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-omdk#6v5tyi)@*n+r@a(rke^4tj73*hv+!4%hjm5y18+%%+(ni'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
